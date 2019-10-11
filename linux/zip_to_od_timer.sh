14af9bc6ab3a09ce4bd9e2d3b9059e5e4d92febe
copyshiroe <copyshiroe@gmail.com>
2019-10-11 13:23:25 +0800

    :bug: Fix variable name

--- linux/zip_to_od_timer.sh
+++ linux/zip_to_od_timer.sh
@@ -4,7 +4,7 @@ IFS=$'\n'
 path="/download/"
 cmpstr="galleryinfo.txt"
 od="od:inbox/"
-time=$(date "+%Y/%m/%d %H:%M:%S")
+time=$(date "+%Y\/%m\/%d %H:%M:%S")
 echo -e "\n[ ${time} ]  ${0} run start" >>${path}rod.log
 for folder in [ ${path}* ];do
     if [ -d "${folder}" ];then
@@ -15,13 +15,13 @@ for folder in [ ${path}* ];do
             #echo "${folder}"
             
             zip -rm "${folder}.zip" "$folder" >>"${path}rod.log" 
-            echo -e "\n"
+            echo -e "------------------------\n" >>${path}rod.log
             rclone move -v "${folder}.zip" "${od}" --transfers=1 --log-file="${path}rod.log"
             echo -e "------------------------\n" >>${path}rod.log
             
         fi
     fi
 done
-${time}=$(date "+%Y/%m/%d %H:%M:%S")
+time=$(date "+%Y\/%m\/%d %H:%M:%S")
 echo -e "\n[ ${time} ]  ${0} run finish" >>${path}rod.log
 echo -e "------------------------------------------------\n" >>${path}rod.log
