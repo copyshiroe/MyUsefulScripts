a691937bac42feca99b7745aec7b9ecf6a12dd8b
copyshiroe <copyshiroe@gmail.com>
2019-10-12 16:49:17 +0800

    :sparkles: Single instance run

--- linux/zip_to_od_timer.sh
+++ linux/zip_to_od_timer.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 #usage:
+[ "${FLOCKER}" != "$0" ] && exec env FLOCKER="$0" flock -en "$0" "$0" "$@" || :
 IFS=$'\n'
 path="/download/"
 cmpstr="galleryinfo.txt"
-od="od:inbox/"
-time=$(date "+%Y\/%m\/%d %H:%M:%S")
-echo -e "\n[ ${time} ]  ${0} run start" >>${path}rod.log
+od="od:inbox/unread"
+echo -e "\n[ ${time} ] /t ${0} run start" >>${path}rod.log
 for folder in [ ${path}* ];do
     if [ -d "${folder}" ];then
         #echo "${folder}"
@@ -14,7 +14,7 @@ for folder in [ ${path}* ];do
         if [[ "${folder}/${cmpstr}" == "$tmp" ]];then
             #echo "${folder}"
             
-            zip -rm "${folder}.zip" "$folder" >>"${path}rod.log" 
+            zip -rm "${folder}.zip" "$folder" 
             echo -e "------------------------\n" >>${path}rod.log
             rclone move -v "${folder}.zip" "${od}" --transfers=1 --log-file="${path}rod.log"
             echo -e "------------------------\n" >>${path}rod.log
@@ -22,6 +22,6 @@ for folder in [ ${path}* ];do
         fi
     fi
 done
-time=$(date "+%Y\/%m\/%d %H:%M:%S")
-echo -e "\n[ ${time} ]  ${0} run finish" >>${path}rod.log
+time=$(date "+%Y/%m/%d %H:%M:%S")
+echo -e "\n[ ${time} ] /t ${0} run finish" >>${path}rod.log
 echo -e "------------------------------------------------\n" >>${path}rod.log
